da9d0666472bea6c33518e23ba5ffcb1e364eb9c
tutntut <97011426+tutntut@users.noreply.github.com>
2023-03-07 09:34:55 +0900

    Add files via upload

---
+++ C/stronglyC/ch2/String01.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(void) {
+    char ch1 = 'A', ch2 = 'B', ch3 = 'C';
+    // Both same result
+    char szData[4] = {'A', 'B', 'C'};
+    char szNewData[4] = {"ABC"};
+
+    return 0;
+}
